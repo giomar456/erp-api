cba8464484de33393503868167f2c5b9b8bf6d88
giomar456 <139256050+giomar456@users.noreply.github.com>
2026-04-29 17:10:45 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import psycopg2
+import os
 
 app = FastAPI()
 
+def get_conn():
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
 @app.get("/")
 def home():
-    return {"msg": "API ERP OK"}
+    return {"msg": "API ERP CON POSTGRES OK"}
+
+@app.get("/test-db")
+def test_db():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT 1")
+    result = cur.fetchone()
+    conn.close()
+
+    return {"db": "ok", "result": result}
