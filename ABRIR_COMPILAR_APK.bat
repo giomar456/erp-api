1cbb5b4d709f25a0bef977377188125d7230972e
giomar456 <139256050+giomar456@users.noreply.github.com>
2026-07-01 12:34:03 -0500

    Limpieza servidor Render: repo minimo + releases viejos

--- ABRIR_COMPILAR_APK.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "compilar_apk_android.ps1"
-pause
--- ABRIR_PUBLICAR_AVANCE.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0publicar_avance_completo.ps1"
-pause
